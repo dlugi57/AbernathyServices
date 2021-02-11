e0579fef87a4ea52cae12ecdbd31ca5b23ecfd02
dlugi57 <dlugoszpeter@gmail.com>
2021-02-11 23:08:56 +0100

    CLEAN and configuration

--- Patient/src/main/java/com/abernathy/patient/model/Patient.java
+++ Patient/src/main/java/com/abernathy/patient/model/Patient.java
@@ -1,6 +1,11 @@
 package com.abernathy.patient.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -8,9 +13,17 @@ import javax.validation.constraints.Size;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-@Data
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+//@Data
+@Getter
+@Setter
+@ToString
 @Entity
 public class Patient {
+
+    //DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,10 +41,12 @@ public class Patient {
     @Size(max = 30, message = "First Name should be maximum 30 characters")
     String lastName;
 
+    //@DateTimeFormat(pattern = "dd/MM/yyyy")
+    @JsonFormat(pattern="dd-MM-yyyy")
+    @NotNull(message = "Birth date is mandatory")
     private LocalDate birthDate;
 
     @NotNull(message = "sex is mandatory")
-    @NotBlank(message = "sex is mandatory")
     private PatientSex sex;
 
     @Column(length = 255)
@@ -41,4 +56,6 @@ public class Patient {
     @Column(length = 13)
     @Size(max = 13, message = "phone should be maximum 13 characters")
     String phone;
+
+
 }
