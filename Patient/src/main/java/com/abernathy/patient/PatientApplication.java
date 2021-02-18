0928c00ba380d7a0937cade8b6477a1c8b33b280
dlugi57 <dlugoszpeter@gmail.com>
2021-02-18 23:24:23 +0100

    DEV PATIENT add some notes

--- Patient/src/main/java/com/abernathy/patient/PatientApplication.java
+++ Patient/src/main/java/com/abernathy/patient/PatientApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PatientApplication {
-
+	// TODO: 18/02/2021
+	//Les utilisateurs peuvent annuler un rendez-vous sans mettre à jour leurs informations
 	public static void main(String[] args) {
 		SpringApplication.run(PatientApplication.class, args);
 	}
