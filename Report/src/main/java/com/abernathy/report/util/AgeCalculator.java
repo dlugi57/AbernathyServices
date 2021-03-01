b836a90c6ce404d464ebc9bc181c41c35ea6868e
dlugi57 <dlugoszpeter@gmail.com>
2021-03-01 23:38:17 +0100

    DEV REPORT age calculator helper

---
+++ Report/src/main/java/com/abernathy/report/util/AgeCalculator.java
@@ -0,0 +1,36 @@
+package com.abernathy.report.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+/**
+ * Convert date formats to age
+ */
+public class AgeCalculator {
+
+    static final Logger logger = LogManager
+            .getLogger(AgeCalculator.class);
+
+    /**
+     * COnvers local date to age
+     *
+     * @param birthDate local date
+     * @return years
+     */
+    public static int calculateAge(LocalDate birthDate) {
+
+        LocalDate currentDate = LocalDate.now();
+        //if ((birthDate != null) && (currentDate != null)) {
+        if (birthDate != null) {
+            return Period.between(birthDate, currentDate).getYears();
+        } else {
+            logger.error("Method: calculateAge /**/ Message: The date is not available");
+            return 0;
+        }
+    }
+
+}
+
