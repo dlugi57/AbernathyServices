02919939a8aabef12c9426d1df8f9333eabe991b
dlugi57 <dlugoszpeter@gmail.com>
2021-03-03 00:27:05 +0100

    DEV report add text to the enums

--- Report/src/main/java/com/abernathy/report/model/ReportStatus.java
+++ Report/src/main/java/com/abernathy/report/model/ReportStatus.java
@@ -1,6 +1,30 @@
 package com.abernathy.report.model;
 
 public enum ReportStatus {
-   NONE, BORDERLINE, IN_DANGER, EARLY_ONSET;
+   //NONE, BORDERLINE, IN_DANGER, EARLY_ONSET;
+
+   NONE("None"),
+   BORDERLINE("Borderline"),
+   IN_DANGER("In danger"),
+   EARLY_ONSET("Early onset");
+
+   private String text;
+
+   ReportStatus(String text) {
+      this.text = text;
+   }
+
+   public String getText() {
+      return this.text;
+   }
+
+/*   public static ReportStatus fromString(String text) {
+      for (ReportStatus b : ReportStatus.values()) {
+         if (b.text.equalsIgnoreCase(text)) {
+            return b;
+         }
+      }
+      return null;
+   }*/
 
 }
